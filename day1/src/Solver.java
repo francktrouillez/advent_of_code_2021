8726576379a77cc0b84a0401dcdd84e03a3a3b05
Franck Trouillez <francktrouillez99@gmail.com>
2022-12-03 03:25:08 +0100

    Solve day 1

---
+++ day1/src/Solver.java
@@ -0,0 +1,50 @@
+package src;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Solver extends Base {
+
+  public Solver() throws IOException{
+    super();
+  }
+
+  public String generateOutput1() {
+    int previousValue;
+    int currentValue;
+    int increasedCounter = 0;
+    List<Integer> integerInput = getIntegerInputList();
+    for (int i = 1; i < integerInput.size(); i++) {
+      previousValue = integerInput.get(i - 1);
+      currentValue = integerInput.get(i);
+      if (previousValue < currentValue) {
+        increasedCounter++;
+      }
+    }
+    return Integer.toString(increasedCounter);
+  }
+
+  public String generateOutput2() {
+    int previousValue;
+    int currentValue;
+    int increasedCounter = 0;
+    List<Integer> integerInput = getIntegerInputList();
+    for (int i = 1; i < integerInput.size() - 2; i++) {
+      previousValue = getSum(integerInput.subList(i - 1, i + 2));
+      currentValue = getSum(integerInput.subList(i, i + 3));
+      if (previousValue < currentValue) {
+        increasedCounter++;
+      }
+    }
+    return Integer.toString(increasedCounter);
+  }
+
+  private List<Integer> getIntegerInputList() {
+    return getInput().stream().map(e -> Integer.parseInt(e)).collect(Collectors.toList());
+  }
+
+  private int getSum(List<Integer> values) {
+    return values.stream().reduce(0, (acc, e) -> acc + e);
+  }
+}
